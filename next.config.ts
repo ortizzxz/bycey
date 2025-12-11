6770b261071e73ab27b2b3eb2752e30ab1d21a16
JesusO <jesus.ortiz@yellow-time.de>
2025-12-11 11:58:22 +0100

    config(next file)

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',            // required for `next export` to generate static HTML
-  basePath: '/your-repo-name', // replace with your GitHub repo name
-  trailingSlash: true,         // ensures proper routing for GitHub Pages
+  /* config options here */
 };
 
 export default nextConfig;

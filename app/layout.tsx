a0f4e9c20af99bc6c1106be0df8e2a1f3bd32d2f
JesusO <jesus.ortiz@yellow-time.de>
2025-12-12 08:48:42 +0100

    meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import NavBarWrapper from "./components/navbarWrapper";
 export const metadata: Metadata = {
   title: "Bycey Excavations",
   description: "Bycey Excavations provides professional earthmoving, excavation, and waterpipe replacement services across Victoria, Australia. Safe, reliable, and efficient solutions for residential, commercial, and municipal projects.",
+   verification: {
+    google: "vZLXFrB9nDHb9eFf8TPWLKy433Ec0L9V1jCwH7tEMk4",
+  }
 
 };
 
@@ -14,9 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="google-site-verification" content="vZLXFrB9nDHb9eFf8TPWLKy433Ec0L9V1jCwH7tEMk4" />
-      </head>
       <body>
         <NavBarWrapper />
         {children}
